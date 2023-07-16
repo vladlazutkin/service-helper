0a3736481c6f95fd9240b81c819dc06808f2dcb5
Vladyslav Lazutkin <vladyslav.l@maybeworks.dev>
2023-07-16 19:04:14 +0300

    wip

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/ping', (req, res) => {
+app.get('/api/v1/ping', (req, res) => {
   res.json({ message: 'alive' });
 });
 
