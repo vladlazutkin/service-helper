7afc0d3712f9793474a44fb1e119d1fe5a8ed7e6
Vladyslav Lazutkin <vladyslav.l@maybeworks.dev>
2023-07-17 12:31:11 +0300

    wip

--- dist/helpers/getUserFromRequest.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUserFromRequest = void 0;
-const getUserFromRequest = (req) => req.user;
-exports.getUserFromRequest = getUserFromRequest;
-//# sourceMappingURL=getUserFromRequest.js.map
