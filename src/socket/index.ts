3a594e417c28daf226ae5cbbb53f74f93a760eea
Vladyslav Lazutkin <vladyslav.l@maybeworks.dev>
2023-07-27 22:51:35 +0300

    init-chess

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -21,24 +21,27 @@ export const io = new Server(server, {
 const rooms: { roomId: string; white: string; black: string | null }[] = [];
 
 io.on('connection', (socket) => {
-  const lastRoom = rooms[rooms.length - 1];
   let roomId = '';
-  if (!lastRoom) {
-    roomId = uuidv4();
-    const newRoom = { roomId, white: socket.id, black: null };
-    rooms.push(newRoom);
-    socket.emit('chess-color', FIGURE_COLOR.WHITE);
-  } else if (!lastRoom.black) {
-    roomId = lastRoom.roomId;
-    lastRoom.black = socket.id;
-    socket.emit('chess-color', FIGURE_COLOR.BLACK);
-  } else {
-    roomId = uuidv4();
-    const newRoom = { roomId, white: socket.id, black: null };
-    rooms.push(newRoom);
-    socket.emit('chess-color', FIGURE_COLOR.WHITE);
-  }
-  socket.join(roomId);
+
+  socket.on('init-chess', () => {
+    const lastRoom = rooms[rooms.length - 1];
+    if (!lastRoom) {
+      roomId = uuidv4();
+      const newRoom = { roomId, white: socket.id, black: null };
+      rooms.push(newRoom);
+      socket.emit('chess-color', FIGURE_COLOR.WHITE);
+    } else if (!lastRoom.black) {
+      roomId = lastRoom.roomId;
+      lastRoom.black = socket.id;
+      socket.emit('chess-color', FIGURE_COLOR.BLACK);
+    } else {
+      roomId = uuidv4();
+      const newRoom = { roomId, white: socket.id, black: null };
+      rooms.push(newRoom);
+      socket.emit('chess-color', FIGURE_COLOR.WHITE);
+    }
+    socket.join(roomId);
+  });
 
   socket.on('chess-move', (data) => {
     console.log('chess event');
